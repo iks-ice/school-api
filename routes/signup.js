fd803f6cfd8ca3df128a978d9baa6dd3bdb723a5
Oleg Parshyn <olegparshyn@MacBook-Pro-LoopMe-3.local>
2020-01-27 16:01:03 +0200

    check signup data

--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,16 @@
 
+const { check, validationResult } = require('express-validator');
 const router = require('express').Router();
 
-router.post('/', (req, res) => {
+router.post('/', [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Email is required').isEmail(),
+  check('password', 'Password is required').isLength({ min: 6 }),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.json(errors);
+  }
   res.json({ msg: 'signed up ' });
 });
 
