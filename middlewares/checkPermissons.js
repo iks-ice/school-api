2beb4cdf04d357ad5d35b2bc03670461ca5832bc
Oleg Parshyn <olegparshyn@MacBook-Pro-LoopMe-3.local>
2020-01-28 16:00:26 +0200

    checkRole middleware

---
+++ middlewares/checkPermissons.js
@@ -0,0 +1,37 @@
+/* eslint-disable consistent-return */
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const checkAuth = async (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return res.json({ msg: 'Authantication failed' });
+  }
+  try {
+    const decoded = await jwt.verify(token, process.env.jwtSecret);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+
+const checkRole = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id);
+    console.log(user);
+    if (user.role === 'teacher') {
+      return next();
+    }
+    return res.status(400).json({ msg: 'Permission denied' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server error' });
+  }
+};
+
+module.exports = {
+  checkRole,
+  checkAuth,
+};
