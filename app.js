bf1832033a073fc31ddbe179eafe81b29ef5b476
Oleg Parshyn <olegparshyn@MacBook-Pro-LoopMe-3.local>
2020-01-29 11:26:53 +0200

    added 404 error handler

--- app.js
+++ app.js
@@ -12,5 +12,9 @@ app.use('/api/signup', require('./routes/signup'));
 app.use('/api/login', require('./routes/login'));
 app.use('/api/lessons', require('./routes/lessons'));
 
+app.use('*', (req, res) => {
+  res.json({ msg: 'Page Not Found' });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
